627006f0dc588de92fcea6d8fe0c3979ce6e3e6a
Findney <agilmughni1@gmail.com>
2025-05-16 15:54:57 +0700

    feat: update main

--- scraping/main.py
+++ scraping/main.py
@@ -2,7 +2,7 @@ import logging
 import importlib
 from scrapers import helper
 
-SOURCES = ["kompas", "tempo"]
+SOURCES = ["kompas", "tempo", "medium"]
 
 
 def show_main_menu():
@@ -17,6 +17,7 @@ def show_source_menu():
     print("\nPilih sumber media:")
     for idx, source in enumerate(SOURCES, start=1):
         print(f"{idx}. {source.capitalize()}")
+    print(f"{len(SOURCES) + 1}. Kembali ke menu utama")
     return input(f"Pilih sumber (1-{len(SOURCES)}): ").strip()
 
 
@@ -26,25 +27,28 @@ def main():
 
         if main_choice in ["1", "2"]:
             source_choice = show_source_menu()
-            if not source_choice.isdigit() or int(source_choice) not in range(
-                1, len(SOURCES) + 1
-            ):
-                print("Pilihan sumber tidak valid.")
+            if source_choice in [str(i) for i in range(1, len(SOURCES) + 1)]:
+                selected_source = SOURCES[int(source_choice) - 1]
+                helper.setup_logging(selected_source)
+
+                action = "Crawl URL" if main_choice == "1" else "Scrape Data"
+                logging.info(f"Opsi dipilih: {action} untuk sumber: {selected_source}")
+
+                try:
+                    scraper_module = importlib.import_module(
+                        f"scrapers.{selected_source}"
+                    )
+                    scraper_module.run(int(main_choice))
+                except Exception as e:
+                    logging.error(
+                        f"Error saat menjalankan {action.lower()} untuk {selected_source}: {e}"
+                    )
+            elif source_choice == str(len(SOURCES) + 1):
+                logging.info("Kembali ke menu utama.")
+                print("Kembali ke menu utama.")
                 continue
-
-            selected_source = SOURCES[int(source_choice) - 1]
-            helper.setup_logging(selected_source)
-
-            action = "Crawl URL" if main_choice == "1" else "Scrape Data"
-            logging.info(f"Opsi dipilih: {action} untuk sumber: {selected_source}")
-
-            try:
-                scraper_module = importlib.import_module(f"scrapers.{selected_source}")
-                scraper_module.run(int(main_choice))
-            except Exception as e:
-                logging.error(
-                    f"Error saat menjalankan {action.lower()} untuk {selected_source}: {e}"
-                )
+            else:
+                print("Pilihan sumber tidak valid.")
 
         elif main_choice == "3":
             logging.info("Keluar dari program.")
