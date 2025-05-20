19700252e82fdf99a4bf7a77963b83b71f7a7187
Findney <agilmughni1@gmail.com>
2025-05-21 03:16:22 +0700

    feat: add import module

--- scraping/scrapers/__init__.py
+++ scraping/scrapers/__init__.py
@@ -1,4 +1,21 @@
 from . import kompas
 from . import helper
+from . import transform
+from . import tempo
+from . import medium
+from . import medium_en
+from . import mojok
+from . import etd_usk
+from . import etd_ugm
 
-__all__ = ["kompas", "helper"]
+__all__ = [
+    "kompas",
+    "helper",
+    "transform",
+    "tempo",
+    "medium",
+    "medium_en",
+    "mojok",
+    "etd_usk",
+    "etd_ugm",
+]
